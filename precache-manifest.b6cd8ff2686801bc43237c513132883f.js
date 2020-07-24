3d7a424f97cfc848d7bd1e8f98a34766b26fd42b
monteiro-alexandre <monteiro-alexandre@users.noreply.github.com>
2020-07-24 03:36:50 +0000

    deploy: f7cadc07ece540636acffeab1d0ce94450f575b5

---
+++ precache-manifest.b6cd8ff2686801bc43237c513132883f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5c7d153715290ed5c9d8b0386648d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd32dcdf726a696428da",
+    "url": "/static/js/2.2b62c9fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2b62c9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182e703d30cf63557e7e",
+    "url": "/static/js/main.4794b2a3.chunk.js"
+  },
+  {
+    "revision": "e169b5ddd8e90c07e068",
+    "url": "/static/js/runtime-main.5edb8bef.js"
+  },
+  {
+    "revision": "052f1d3a59241f175a524ddb88306820",
+    "url": "/static/media/alexandre-monteiro.052f1d3a.png"
+  },
+  {
+    "revision": "d257dc26de972a1a83ceb4b011f96622",
+    "url": "/static/media/background-1.d257dc26.svg"
+  },
+  {
+    "revision": "580f6de124bfc95ab0c5416a785af3ac",
+    "url": "/static/media/background-2.580f6de1.svg"
+  },
+  {
+    "revision": "ffe3f788fdebf5a8d226c784dcf45853",
+    "url": "/static/media/background-3.ffe3f788.svg"
+  },
+  {
+    "revision": "f43f76c415ca2d3d4d88c8a3d3f5ebe4",
+    "url": "/static/media/background-4.f43f76c4.svg"
+  },
+  {
+    "revision": "f7afb745179748ede2ed5d2dbe54ef3c",
+    "url": "/static/media/background-5.f7afb745.svg"
+  },
+  {
+    "revision": "bd46bcf97a5b964bad681f92a32b1415",
+    "url": "/static/media/background-6.bd46bcf9.svg"
+  },
+  {
+    "revision": "b53c02f0a26617a21d6f92c60537be64",
+    "url": "/static/media/banner-background.b53c02f0.jpg"
+  },
+  {
+    "revision": "04a46e40eaf4c3444c7c2b625b13c10a",
+    "url": "/static/media/footer-background.04a46e40.jpg"
+  },
+  {
+    "revision": "1021a8a25da0215bb8fe94f9540cac57",
+    "url": "/static/media/undraw_avatar_female.1021a8a2.png"
+  },
+  {
+    "revision": "ff890f4383d33244b5ab6e16f4baa35a",
+    "url": "/static/media/undraw_avatar_male.ff890f43.png"
+  },
+  {
+    "revision": "ef716148a400157a531aeb1bfb9a6372",
+    "url": "/static/media/undraw_avatar_profile.ef716148.png"
+  },
+  {
+    "revision": "0388d6537be26a264be7b2fc94ba3412",
+    "url": "/static/media/undraw_code_review.0388d653.svg"
+  },
+  {
+    "revision": "099b285fc6628d92e4238f4f32c15c23",
+    "url": "/static/media/undraw_landing_page.099b285f.svg"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/undraw_programmer.9a76e74e.svg"
+  },
+  {
+    "revision": "a80610c5cebf13ceaf55ead41172299a",
+    "url": "/static/media/undraw_react.a80610c5.svg"
+  },
+  {
+    "revision": "e79e8c37ddfa35dde18ba5ee1f7a6908",
+    "url": "/static/media/undraw_responsive.e79e8c37.svg"
+  },
+  {
+    "revision": "dbe101302b649c1e2058603f55a9b455",
+    "url": "/static/media/undraw_static_website.dbe10130.svg"
+  },
+  {
+    "revision": "c0c14ee99576af374822c1122cc87e85",
+    "url": "/static/media/undraw_team_spirit.c0c14ee9.svg"
+  }
+]);
